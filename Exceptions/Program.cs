b527d3354e0944a586a2d2a06c23b9a35ccd553d
stastpa20 <stastpa20@sps-prosek.cz>
2023-02-28 11:51:35 +0100

    Přidejte soubory projektu.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,46 @@
+﻿int cisloA = 0;
+int cisloB = 0;
+
+bool aJo = int.TryParse(Console.ReadLine(), out cisloA);
+bool bJo = int.TryParse(Console.ReadLine(), out cisloB);
+
+/*try
+{
+    Console.WriteLine(cisloA / cisloB);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.ReadLine();
+}*/
+
+try
+{
+    if(cisloB == 0) { throw new MyException("deleni nulou"); }
+    Console.WriteLine(cisloA / cisloB);
+}
+catch (MyException ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.ReadLine();
+}
+
+public class MyException : Exception
+{
+    public MyException()
+    {
+    }
+
+    public MyException(string message)
+        : base(message)
+    {
+    }
+
+    public MyException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+
+}
+
+
